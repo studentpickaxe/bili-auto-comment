6da5971bb8436644b3a406821b54e64a436e2470
irasutoya <iqboost@icloud.com>
2026-01-29 20:55:04 +0800

    Update User-Agent header for HTTP request

--- src/main/java/yfrp/autobili/vid/BiliApi.java
+++ src/main/java/yfrp/autobili/vid/BiliApi.java
@@ -20,7 +20,7 @@ public class BiliApi {
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder()
                 .uri(URI.create(apiUrl))
-                .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.0.0")
                 .GET()
                 .build();
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
