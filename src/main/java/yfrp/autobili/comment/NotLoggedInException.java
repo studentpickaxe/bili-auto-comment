ba51892dc385891cd6947c0ccdfef97b3f5251eb
stu <naqiakvufqasqwemcax@outlook.com>
2026-01-30 15:19:25 +0800

    未登录异常

---
+++ src/main/java/yfrp/autobili/comment/NotLoggedInException.java
@@ -0,0 +1,27 @@
+package yfrp.autobili.comment;
+
+/**
+ * 未登录异常
+ * <p>
+ * 当被强制退出登录时抛出此异常
+ */
+public class NotLoggedInException
+        extends Exception {
+
+    /**
+     * 构造一个无参数的未登录异常
+     */
+    public NotLoggedInException() {
+        super();
+    }
+
+    /**
+     * 构造一个带消息的未登录异常
+     *
+     * @param message 异常消息
+     */
+    public NotLoggedInException(String message) {
+        super(message);
+    }
+
+}
